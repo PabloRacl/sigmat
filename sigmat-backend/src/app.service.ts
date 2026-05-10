2af9df8a6d058a5443eb852bac74e55378d29238
PabloRacl <pabloracl@gmail.com>
2026-05-09 23:26:32 -0300

    Debug: add version tag to root

--- sigmat-backend/src/app.service.ts
+++ sigmat-backend/src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   obterSaudacao(): string {
-    return 'Olá Mundo! - SIGMAT PMPE API rodando.';
+    return 'Olá Mundo! - SIGMAT PMPE API rodando (v1.0.3).';
   }
 }
 
