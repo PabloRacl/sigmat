bee7632f797bc2fb86017b34da76f04299fac1e2
PabloRacl <152287830+PabloRacl@users.noreply.github.com>
2026-06-03 12:39:54 -0300

    Delete login-implantar directory

--- login-implantar/get-token.js
+++
@@ -1,8 +0,0 @@
-const getToken = (req) => {
-
-  const authHeader = req.headers.authorization
-  const token =  authHeader && authHeader.split(" ")[1]
-  return token
-}
-
-module.exports = getToken
